60ed897a640db16f5f1476229916f4d772dafd54
AmanTechInfo1 <114477488+AmanTechInfo1@users.noreply.github.com>
2024-06-05 00:38:35 +0530

    Delete SmartLearner.WEB directory

--- SmartLearner.WEB/src/formSchemas/category/index.jsx
+++
@@ -1,6 +0,0 @@
-import * as yup from "yup";
-
-export const categorySchema = yup.object({
-    name: yup.string().required("Name is required").trim(),
-    description: yup.string().nullable().trim(),
-}); 
--- SmartLearner.WEB/src/formSchemas/postcode/index.js
+++
@@ -1,7 +0,0 @@
-import * as yup from "yup";
-
-export const postcodeSchema = yup.object({
-    postcode: yup.string().required("Name is required").trim(),
-    city: yup.string().required("City is required").trim(),
-    country: yup.string().required("Country is required").trim()
-}); 
