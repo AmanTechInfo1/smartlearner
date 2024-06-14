fc9587181056839a9ceea9b563c43642154a3881
AmanTechInfo1 <amanchandel2620@gmail.com>
2024-06-14 08:25:25 +0530

    elc-Page

--- clientapp/src/pages/CorporateResponsbilites/ElectricCarScheme.js
+++ clientapp/src/pages/CorporateResponsbilites/ElectricCarScheme.js
@@ -1,7 +1,13 @@
 import React from "react";
 import styles from "./ElectricCar.module.css";
-import electricCar from '../../assets/images/electric-Car.png'
-import electricCarScheme from '../../assets/images/electric carScheme.png'
+import electricCar from "../../assets/images/electric-Car.png";
+import electricCarScheme from "../../assets/images/electric carScheme.png";
+import treesImgs from "../../assets/images/trees-img.png";
+import BuildingImg from "../../assets/images/buliding img.png";
+import treeImg from "../../assets/images/tree img.png";
+import { Link } from "react-router-dom";
+
+
 export default function ElectricCarScheme() {
   return (
     <div className={styles.ElectricCarScheme}>
@@ -48,6 +54,113 @@ export default function ElectricCarScheme() {
             </div>
           </section>
         </section>
+        {/* /////////////////////////////////////////// */}
+        <section className={styles.textMainSection}>
+          <section className={styles.textEscsection}>
+            <p>
+              Thanks to Coventry city council's support we're leading the way
+              towards a healthier environment.
+              <span>
+                {" "}
+                SmartLearner is proud to be the first driving school in the West
+                Midlands with an electric car.
+              </span>
+            </p>
+          </section>
+          <section className={styles.treeImgsSection}>
+            <img src={treesImgs} alt="trees-Img" />
+            <img src={treesImgs} alt="trees-Img" />
+            <img src={treesImgs} alt="trees-Img" />
+          </section>
+          <hr />
+        </section>
+
+        {/* ////////////////////////////////////// */}
+        <section className={styles.escBDSection}>
+          <div className={styles.escBdHeading}>
+            <h2>ELECTRIC ENGINES</h2>
+          </div>
+          <section className={styles.escBenifits}>
+            <div className={styles.escBDDiv}>
+              <section>
+                <h2>BENEFITS</h2>
+                <p>
+                  Electrically powered - electricity can be renewable resource
+                  gasoline cannot.
+                </p>
+                <p>Electric engines do not release co2 emissions.</p>
+                <p>
+                  Annual tax and maintenance costs (including MOTs and
+                  servicing) for electric vehicles are 49% lower than for petrol
+                  models.
+                </p>
+              </section>
+            </div>
+            <div className={styles.escBDDiv}>
+              <div>
+                {" "}
+                <h2>DISADVANTAGES</h2>
+                <p>Recharging the battery takes times</p>
+                <p>It can sometimes difficult to find a charging station.</p>
+                <p>
+                  Insurance costs are on average 25% higher for electric
+                  vehicles.
+                </p>
+              </div>
+            </div>
+          </section>
+        </section>
+        {/* /////////////////////////////////// */}
+        <section className={styles.escBDSection}>
+          <div className={styles.escBdHeading}>
+            <h2>COMBUSTION ENGINES</h2>
+            <hr/>
+          </div>
+          <section className={styles.escBenifits}>
+            <div className={styles.escBDDiv}>
+              <section>
+                <h2>BENEFITS</h2>
+                <p>Gas powered cars have more power.</p>
+                <p>Have better agility in terms of acceleration and speed.</p>
+                <p>Can be refilled quickly.</p>
+              </section>
+            </div>
+            <div className={styles.escBDDiv}>
+              <div>
+                {" "}
+                <h2>DISADVANTAGES</h2>
+                <p>
+                  Gas powered vehicles emit harmful emissions causing pollution
+                  to our atmosphere.
+                </p>
+                <p>
+                  The cost of fuel is expensive. For an electric car to travel
+                  100 miles would cost around 4p per mile in comparison to gas
+                  powered car costing 9p per mile. fuel costing fuel costing £5
+                  more.
+                </p>
+              </div>
+            </div>
+          </section>
+        </section>
+        {/* ///////////////////////////////////////////// */}
+        <section className={styles.escBDSection}>
+          <div className={styles.escBdHeading}>
+            <h2>OUR OTHER PROJECTS</h2>
+            <hr/>
+          </div>
+        <section className={styles.csr3rdDivSecLinks}>
+           
+            <Link>
+              <img src={treeImg} alt="eclectric-Img" />
+              <p>Going Green Project</p>
+            </Link>
+            <Link>
+              <img src={BuildingImg} alt="eclectric-Img" />
+              <p>Our Office Efforts</p>
+            </Link>
+          </section>
+          </section>
       </div>
     </div>
   );
