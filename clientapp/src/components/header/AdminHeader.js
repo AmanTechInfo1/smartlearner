b1fed8f4bf48c5fcf6951c76077450daebbcd8b3
AmanTechInfo1 <amanchandel2620@gmail.com>
2024-06-22 09:58:41 +0530

    push

--- clientapp/src/components/header/AdminHeader.js
+++ clientapp/src/components/header/AdminHeader.js
@@ -1,23 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import styles from "../../assets/css/admin.module.css";
-import { BsFillBellFill, BsFillEnvelopeFill, BsPersonCircle, BsSearch, BsJustify } from 'react-icons/bs';
-import { Link } from 'react-router-dom';
+import {
+  BsFillBellFill,
+  BsFillEnvelopeFill,
+  BsPersonCircle,
+  BsSearch,
+  BsJustify,
+} from "react-icons/bs";
+import { Link, Navigate, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { logoutUser } from "../../redux/features/authSlice";
 
 function AdminHeader({ OpenSidebar }) {
-    const [searchQuery, setSearchQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState("");
 
-    const performSearch = () => {
-        console.log('Search initiated for:', searchQuery);
-        // Perform actual search operation here
-    };
-
-    return (
-        <>
-            <header className={styles.adminHeader}>
-                <div className={styles.adminMenuIcon}>
-                    <BsJustify className={styles.adminIcon} onClick={OpenSidebar} />
-                </div>
-                {/* <div className={styles.searchContainer}>
+  const performSearch = () => {
+    console.log("Search initiated for:", searchQuery);
+    // Perform actual search operation here
+  };
+  const dispatch = useDispatch();
+const navigate = useNavigate();
+  return (
+    <>
+      <header className={styles.adminHeader}>
+        <div className={styles.adminMenuIcon}>
+          <BsJustify className={styles.adminIcon} onClick={OpenSidebar} />
+        </div>
+        {/* <div className={styles.searchContainer}>
                     <input
                         type="text"
                         placeholder="Search..."
@@ -28,13 +37,23 @@ function AdminHeader({ OpenSidebar }) {
                     />
                     <BsSearch className={styles.searchIcon} onClick={performSearch} />
                 </div> */}
-                <div className={styles.adminHeaderRight} style={{textAlign:'right',width:'100%'}}>
-                    
-                    <Link to='/login' ><BsPersonCircle className={styles.adminIcon1} />LogOut</Link>
-                </div>
-            </header>
-        </>
-    )
+        <div
+          className={styles.adminHeaderRight}
+          style={{ textAlign: "right", width: "100%" }}
+        >
+          <span
+            onClick={() => {
+              dispatch(logoutUser());
+              navigate('/');
+            }}
+          >
+            <BsPersonCircle className={styles.adminIcon1} />
+            LogOut
+          </span>
+        </div>
+      </header>
+    </>
+  );
 }
 
-export default AdminHeader
+export default AdminHeader;
