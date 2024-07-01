5af6d4148fa7cc953c3d98557dd09f304fd72397
123am <mgadia29@gmail.com>
2024-07-01 11:20:59 +0530

    Update constants.js

--- clientapp/src/utils/constants.js
+++ clientapp/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const baseUrl="http://localhost:5000";
-// export const baseUrl="https://idyllic-starship-9d834b.netlify.app";
-export const imageBaseUrl="http://localhost:5000/uploads/";
-// export const imageBaseUrl="https://idyllic-starship-9d834b.netlify.app/uploads/";
+// export const baseUrl="http://localhost:5000";
+export const baseUrl="https://idyllic-starship-9d834b.netlify.app";
+// export const imageBaseUrl="http://localhost:5000/uploads/";
+export const imageBaseUrl="https://idyllic-starship-9d834b.netlify.app/uploads/";
