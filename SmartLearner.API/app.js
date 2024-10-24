f2da95fab55c4896437afeb8b14bc65417d1b73f
AmanTechInfo1 <114477488+AmanTechInfo1@users.noreply.github.com>
2024-10-24 18:52:37 +0530

    push

--- SmartLearner.API/app.js
+++ SmartLearner.API/app.js
@@ -26,13 +26,16 @@ connectDB();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(cors());
-app.use(cors({
-  origin: 'https://web.smartlearner.com',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(
+  cors({
+    origin: "https://web.smartlearner.com",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
+app.options("*", cors());
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/uploads", (req, res, next) => {
