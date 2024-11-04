107d4d14f6a5b663c3f902ec0878dcfc8ac2c305
AmanTechInfo1 <amanchandel2620@gmail.com>
2024-11-04 16:02:16 +0530

    push

--- SmartLearner.API/app.js
+++ SmartLearner.API/app.js
@@ -31,7 +31,7 @@ app.use((req, res, next) => {
   console.log(`Request Method: ${req.method}, Request URL: ${req.url}`);
   next();
 });
-// Example using Express.js
+
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/uploads", (req, res, next) => {
