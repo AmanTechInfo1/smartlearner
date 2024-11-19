786473cc847ef484dca713e9d0654252a2d1872d
AmanTechInfo1 <amanchandel2620@gmail.com>
2024-11-20 00:26:47 +0530

    push

--- clientapp/src/pages/shop/checkout/PaymentProcessing.js
+++ clientapp/src/pages/shop/checkout/PaymentProcessing.js
@@ -108,7 +108,7 @@ export default function PaymentProcessing() {
         <input type="hidden" name="ekashu_card_email_address_mandatory" value="false" />
 
        
-          <input type="hidden" name="ekashu_hash_code" id="ekashu_hash_code" value={hashcoding} />
+          {/* <input type="hidden" name="ekashu_hash_code" id="ekashu_hash_code" value={hashcoding} /> */}
         
         <input type="hidden" name="ekashu_hash_code_type" id="ekashu_hash_code_type" value="SHA256HMAC" />
         <input type="hidden" name="ekashu_hash_code_version" id="ekashu_hash_code_version" value="2.0.0" />
