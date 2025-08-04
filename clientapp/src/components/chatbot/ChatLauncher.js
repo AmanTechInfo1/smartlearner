f3477c760120b2475ecce7739e681c04aef2bdcd
AmanTechInfo1 <114477488+AmanTechInfo1@users.noreply.github.com>
2025-08-04 21:59:34 +0530

    PUAH

--- clientapp/src/components/chatbot/ChatLauncher.js
+++ clientapp/src/components/chatbot/ChatLauncher.js
@@ -1,13 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Chatbot from "./Chatbot";
 import "./ChatLauncher.css";
 import { FaCommentDots } from "react-icons/fa";
-
+import poppupImg from "../../assets/images/IMG_2423.JPG";
 const ChatLauncher = () => {
   const [isOpened, setIsOpen] = useState(false);
+  const [showPopup, setShowPopup] = useState(false);
+
+  useEffect(() => {
+    setShowPopup(true);
+  }, []);
 
   const toggleChat = () => {
-    if (isOpened) {
+    if (!isOpened) {
+      setShowPopup(false); // Hide popup when user opens the chat
+    } else {
       localStorage.removeItem("sessionId");
       console.log("❌ sessionId removed on close");
     }
@@ -16,7 +23,18 @@ const ChatLauncher = () => {
 
   return (
     <>
-      <div onClick={() => setIsOpen((prev) => !prev)} />
+      {/* Popup */}
+      {showPopup && !isOpened && (
+        <div className="chat-popup">
+          <div className="chat-popup-arrow" />
+          <p>
+            👋 Welcome to the <strong>SmartLearner</strong> live chat,
+            <br />
+            how can we help you today?
+          </p>
+          <img src={poppupImg} alt="popup" />
+        </div>
+      )}
 
       {isOpened && (
         <div className="chat-window">
