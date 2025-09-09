299066bc2fc456587a5d5cd165a3d986773da88e
AmanTechInfo1 <114477488+AmanTechInfo1@users.noreply.github.com>
2025-09-10 02:22:39 +0530

    push

--- clientapp/src/components/chatbot/Chatbot.js
+++ clientapp/src/components/chatbot/Chatbot.js
@@ -418,6 +418,8 @@ const Chatbot = () => {
           }
         );
         const dataA = await res.json();
+        console.log("adas", res);
+        console.log("sdff", dataA);
         const rawText = await res.text(); // 👈 get raw response first
         console.log("📡 Raw SpeechNotes response:", rawText);
 
