54230eda25eca9eca7a80b41911fb2b46874aafb
AmanTechInfo1 <114477488+AmanTechInfo1@users.noreply.github.com>
2025-09-25 19:44:03 +0530

    push

--- SmartLearner.API/services/userSubscriptionService.js
+++ SmartLearner.API/services/userSubscriptionService.js
@@ -759,8 +759,8 @@ class UserSubscriptionService {
     `;
 
     const mailOptions = {
-      from: "admin@smartlearner.com",
-      to: [user.email, "admin@smartlearner.com"],
+      from: "sranchandel@gmail.com",
+      to: [user.email, "sranchandel@gmail.com"],
       subject: `Subscription ${status} - Plan: ${subscription.planname}`,
       html: htmlContent,
     };
