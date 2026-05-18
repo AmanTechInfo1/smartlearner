385a5999dde77e88e715e5b7dc4cb3829d1aac3a
AmanTechInfo1 <sranchandel@gmail.com>
2026-05-18 11:14:27 +0530

    push

--- SmartLearner.API/config/stripe.js
+++ SmartLearner.API/config/stripe.js
@@ -1,3 +1,4 @@
 // backend/config/stripe.js
 const stripe = require("stripe")("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");
 module.exports = stripe;
+//////dsdsad
